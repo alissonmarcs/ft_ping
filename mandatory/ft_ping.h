fa05d39e03eecbc69c4788d852e62635fe16407d
Alisson <alissonmarcos249@gmail.com>
2026-03-22 11:29:50 -0300

    rabisco endless_loop()

--- mandatory/ft_ping.h
+++ mandatory/ft_ping.h
@@ -18,16 +18,27 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <signal.h>
+#include <stdio.h>
+#include <errno.h>
+
 # define ECHO_REQUEST_BUFFER_SIZE 1500
 
 char * build_echo_request();
 void read_echo_reply(char * buffer, size_t size);
+void endless_loop();
 
 typedef struct ping
 {
     int seq;
     pid_t pid;
+    int socket;
+    char recv_buffer[100];
+    char sendto_buffer[100];
+
 } ft_ping;
+
+extern ft_ping ping;
 
 
 #endif
--- mandatory/main.c
+++ mandatory/main.c
@@ -1,118 +1,117 @@
-#include <stdio.h>
-#include <errno.h>
 #include "ft_ping.h"
 
 // ICMP_ECHO = 8
 
+ft_ping ping;
+
+void handler(int sig)
+{
+    (void) sig;
+    printf("OII\n");
+    alarm(1);
+}
+
 int main()
 {
-    ft_ping ping;
+    struct sigaction config_handler;
 
-    memset(&ping, '\0', sizeof (ft_ping));
+    memset(&config_handler, '\0', sizeof(config_handler));
+    config_handler.sa_handler = handler;
 
-    // struct timeval a = {
-    //     .tv_sec = 43,
-    //     .tv_usec = 100
-    // };
-    
-    // struct timeval b = {
-    //     .tv_sec = 42,
-    //     .tv_usec = 101
-    // };
+    sigaction(SIGALRM, &config_handler, NULL);
 
+    handler(42);
 
-    // struct timeval result = {0};
+    while (1)
+        ;
 
-    // timersub(&a, &b, &result);
+    // ft_ping ping;
 
-    // printf("sec: %ld\n", result.tv_sec);
-    // printf("usec: %d\n", result.tv_usec);
+    // memset(&ping, '\0', sizeof (ft_ping));
 
-    // printf("MACRO: %d\n", IPPROTO_ICMP);
+    // int socket_fd;
 
-    int socket_fd;
+    // char *buffer = build_echo_request();
 
-    char *buffer = build_echo_request();
+    // struct addrinfo hints;
+    // memset(&hints, '\0', sizeof (struct addrinfo));
+    // hints.ai_family = AF_INET;
+    // hints.ai_socktype = SOCK_RAW;
+    // hints.ai_flags = 0;
+    // hints.ai_protocol = IPPROTO_ICMP;
 
-    struct addrinfo hints;
-    memset(&hints, '\0', sizeof (struct addrinfo));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_RAW;
-    hints.ai_flags = 0;
-    hints.ai_protocol = IPPROTO_ICMP;
+    // struct addrinfo *result = NULL;
+    // int getaddrinfo_ret;
+    // char * host_name = "google.com";
 
-    struct addrinfo *result = NULL;
-    int getaddrinfo_ret;
-    char * host_name = "google.com";
+    // getaddrinfo_ret = getaddrinfo(host_name, NULL, &hints, &result);
+    // if (getaddrinfo_ret < 0)
+    // {
+    //     perror("getaddrinfo");
+    //     printf("error: %s\n", gai_strerror(errno));
+    //     exit(42);
+    // }
 
-    getaddrinfo_ret = getaddrinfo(host_name, NULL, &hints, &result);
-    if (getaddrinfo_ret < 0)
-    {
-        perror("getaddrinfo");
-        printf("error: %s\n", gai_strerror(errno));
-        exit(42);
-    }
+    // struct addrinfo *current;
+    // for (current = result; current != NULL; current = current->ai_next)
+    // {
+    //     if (current->ai_family == AF_INET)
+    //     {
+    //        break; 
+    //     }
+    //     else
+    //     {
+    //         printf("familia nao suportada: %d\n", current->ai_family);
+    //     }
 
-    struct addrinfo *current;
-    for (current = result; current != NULL; current = current->ai_next)
-    {
-        if (current->ai_family == AF_INET)
-        {
-           break; 
-        }
-        else
-        {
-            printf("familia nao suportada: %d\n", current->ai_family);
-        }
+    // }
+    // if (current == NULL)
+    // {
+    //     printf("nao encontramos ip para o host\n");
+    //     exit(42);
+    // }
+    // struct addrinfo *dest_node = current;
 
-    }
-    if (current == NULL)
-    {
-        printf("nao encontramos ip para o host\n");
-        exit(42);
-    }
-    struct addrinfo *dest_node = current;
-
-    // printf("dest_node->ai_socktype == : %d\n") 
+    // // printf("dest_node->ai_socktype == : %d\n") 
 
 
-    socket_fd = socket(dest_node->ai_family, dest_node->ai_socktype, dest_node->ai_protocol);
-    if (socket_fd < 0)
-    {
-        char * err = "erro na criacao do sock\n";
+    // socket_fd = socket(dest_node->ai_family, dest_node->ai_socktype, dest_node->ai_protocol);
+    // if (socket_fd < 0)
+    // {
+    //     char * err = "erro na criacao do sock\n";
 
-        write (2, err, strlen(err));
-        exit (42);
-    }
+    //     write (2, err, strlen(err));
+    //     exit (42);
+    // }
 
 
-    struct sockaddr * dest_addr = current->ai_addr; 
-    socklen_t dest_addr_len = current->ai_addrlen;
+    // struct sockaddr * dest_addr = current->ai_addr; 
+    // socklen_t dest_addr_len = current->ai_addrlen;
 
-    ssize_t ret = sendto(socket_fd, buffer, 64, 0,  dest_addr, dest_addr_len);
-    if (ret < 0)
-    {
-        perror("sendto");
-        printf("errno: %d\n", errno);
-        exit(42);
-    }
+    // ssize_t ret = sendto(socket_fd, buffer, 64, 0,  dest_addr, dest_addr_len);
+    // if (ret < 0)
+    // {
+    //     perror("sendto");
+    //     printf("errno: %d\n", errno);
+    //     exit(42);
+    // }
 
-    printf("AQUII\n");
+    // printf("AQUII\n");
 
-    char recv_buffer[1024];
-    struct sockaddr_in recv_addr;
-    socklen_t recv_len = sizeof(recv_addr);
+    // char recv_buffer[1024];
+    // struct sockaddr_in recv_addr;
+    // socklen_t recv_len = sizeof(recv_addr);
 
-    ssize_t bytes_received = recvfrom(socket_fd, recv_buffer, sizeof(recv_buffer), 0,
-                                      (struct sockaddr *)&recv_addr, &recv_len);
+    // ssize_t bytes_received = recvfrom(socket_fd, recv_buffer, sizeof(recv_buffer), 0,
+    //                                   (struct sockaddr *)&recv_addr, &recv_len);
 
-    if (bytes_received < 0)
-    {
-        char *err = "erro ao receber resposta\n";
-        write(2, err, strlen(err));
-        exit(42);
-    }
+    // if (bytes_received < 0)
+    // {
+    //     char *err = "erro ao receber resposta\n";
+    //     write(2, err, strlen(err));
+    //     exit(42);
+    // }
 
-    printf("Recebidos %ld bytes de %s\n", bytes_received, inet_ntoa(recv_addr.sin_addr));
-    read_echo_reply(recv_buffer, bytes_received);
+    // printf("Recebidos %ld bytes de %s\n", bytes_received, inet_ntoa(recv_addr.sin_addr));
+    // read_echo_reply(recv_buffer, bytes_received);
 }
