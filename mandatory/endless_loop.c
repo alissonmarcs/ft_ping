bbe03a7aa78f6235e64856cb84d7bea2ba08fa28
alisson <alissonmarcos249@gmail.com>
2026-03-28 18:12:04 -0300

    remocao comentario

--- mandatory/endless_loop.c
+++ mandatory/endless_loop.c
@@ -5,13 +5,11 @@ void endless_loop()
 {
     ssize_t bytes_received;
 
-    while ("I LOVE 42 FOREVER !!!!")
+    while (1)
     {
         if ((bytes_received = recvfrom(ping.socket_fd, ping.raw_echo_reply, ECHO_REQUEST_SIZE, 0, NULL, NULL)) < 0) 
             FATAL_ERROR("recvfrom()");
 
         read_echo_reply(ping.raw_echo_reply, bytes_received);
-
-        // printf("%ld bytes from %s, time=%.2f ms\n", bytes_received, ip_addres_str, ping.current_rtt);
     }
 }
